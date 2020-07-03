ebd92301a3529758aea12d324f29df7ff7731dd6
Girija Shankar Swain <girijashankar.swain@aegonlife.com>
2020-07-03 19:33:09 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-on-digital-ocean-droplet
---
+++ spring-boot-web-app-demo/src/main/java/com/bela/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.bela.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RestController("/")
+	class HelloWorld {
+		@GetMapping("hello")
+		public String hello(){
+			return "Hi, Bela!";
+		}
+	}
+}
+
+
+
+
